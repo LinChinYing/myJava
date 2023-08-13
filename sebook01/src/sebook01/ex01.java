abcd9058ce6fae9835b1193b80cdf1df7ee3f2ec
LinChinYing <piglin0223@gmail.com>
2023-08-13 15:21:41 +0800

    08131521

--- sebook01/src/sebook01/ex01.java
+++ sebook01/src/sebook01/ex01.java
@@ -1,10 +1,16 @@
 package sebook01;
 
+import java.util.Scanner;
+
 public class ex01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("I am Dennis.");
+		Scanner scn = new Scanner(System.in);
+		System.out.println("輸入：");
+		String strName = scn.next();
+		System.out.println("Hi"+strName);
+		scn.close();
 	}
 
 }
