ee493a9ef5eb8a6feb5ad6df6e1bd1bbe5d6a3f5
LinChinYing <piglin0223@gmail.com>
2023-10-26 15:58:44 +0800

    11210261558

---
+++ sebook01/src/Chapter0/Every3.java
@@ -0,0 +1,21 @@
+package Chapter0;
+
+public class Every3 {
+
+	public static void printEvery3() {
+		for(int i = 1; i <= 88; i += 3) {
+			System.out.println(i);
+		}
+	}
+	
+	public static void main(String[] args) {
+	    printEvery3();
+	}
+
+}
+
+//1
+//4
+//7
+//...
+//88
